fb833df4e4efb45aa3a3ab02b73b156cc9d1b6e0
ddddam1 <50946249+ddddam1@users.noreply.github.com>
2020-02-11 00:25:48 +0900

    Home 추가

--- client/src/index.js
+++ client/src/index.js
@@ -4,15 +4,4 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
-
-
-
-var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
-ReactDOM.render(element, document.getElementById('root'));
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById("root"));
