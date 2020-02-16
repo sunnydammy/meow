d718ab3cf32eb9c2af669d585f0ef70c040f05ac
sunnydammy <devshyoo@gmail.com>
2020-02-16 20:36:30 +0900

    commit

---
+++ server/database/create_table.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE OUR_DB;
+
+USE OUR_DB;
+
+-- DROP TABLE USER;
+CREATE TABLE USER
+(
+    ID                  INT            UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    NICK_NAME           VARCHAR(255)   NOT NULL,                         
+    EMAIL_ADDR          VARCHAR(50)    NOT NULL,            
+    PWD                 VARCHAR(255),                       
+    KAKAO_ID            VARCHAR(255),                        
+    KAKAO_ACCESS_TOKEN  VARCHAR(255),                        
+    KAKAO_REFRESH_TOKEN VARCHAR(255),                        
+    FRST_REGS_DTM       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    LAST_REGS_DTM       TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+insert into user (
+    nick_name,
+    email_addr,
+    pwd
+)
+values (
+    'test',
+    'test@naver.com',
+    'test'
+);
--- server/server.js
+++ server/server.js
@@ -1,13 +1,33 @@
-const express = require('express');
+const express    = require('express');
 const bodyParser = require('body-parser');
-const app = express();
-const port = process.env.PORT || 5000;
+const app        = express();
+const port       = process.env.PORT || 5000;
+
+const mysql      = require('mysql');
+const db_config  = require('./config/database');
+
+const db_con     = mysql.createConnection(db_config);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// localhost:5000/
+// app.get('/', (req, res) => res.send('Sorry, this is not the web page you are looking for.'));
+
+
 // kakao_auth
-var kakao_auth = require('./routers/kakao_auth');
+const kakao_auth = require('./routers/kakao_auth');
 app.use('/kakao_auth', kakao_auth);
+
+app.get('/', function(req, res){
+
+    db_con.query('select * from user', function(err, rows) {
+      if(err) throw err;
+  
+      console.log('The solution is: ', rows);
+      res.send(rows);
+
+    });
+  });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
